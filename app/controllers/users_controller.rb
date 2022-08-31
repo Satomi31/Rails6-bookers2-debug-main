c2dc332f70ca5fccc944a680ad4b361dc17ebf86
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-08-31 15:38:43 +0000

    [Add]応用課題7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    # @books_count = @books.page(params[:page]).reverse_order
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
     @book = Book.new
     @followings = @user.followings
     @followers = @user.followers
