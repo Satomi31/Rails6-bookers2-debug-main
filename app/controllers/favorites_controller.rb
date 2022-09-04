90eedd4656bc2a7cc82481f4f4dc69511efe8cab
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-09-04 15:34:11 +0000

    [Add] Boosのshowページのレイアウト調整

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,12 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.new(book_id: @book.id)
     @favorite.save
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+      sort_by {|x|
+        x.favorites.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     # redirect_back(fallback_location: root_path)
   end
 
@@ -10,6 +16,12 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).
+      sort_by {|x|
+        x.favorites.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     # redirect_back(fallback_location: root_path)
   end
 end
